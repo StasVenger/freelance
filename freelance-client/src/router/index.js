cea1088f57ff29cb2e57d2196bd6108267655887
Stanislav Venger <SVenger@actsoft.com>
2022-01-11 21:51:24 +0600

    Created routes for navigation

--- freelance-client/src/router/index.js
+++ freelance-client/src/router/index.js
@@ -1,10 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Tasks from "@/views/Tasks";
+import New from "@/views/New";
+import Task from "@/views/Task";
 
-const routes = [];
+const routes = [
+  { path: "/", name: "Tasks", component: Tasks },
+  { path: "/new", name: "New_Task", component: New },
+  { path: "/:id", name: "Task", component: Task },
+  { path: "/:notFound(.*)*", name: "NotFound" },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkActiveClass: "active",
+  linkExactActiveClass: "active",
 });
 
 export default router;
